fa0a5cb274c8c45ceb982c182f8a2fda5e644d5d
Thanh <Thanh@10.0.0.158>
2019-10-08 20:18:17 -0700

    NEW SAVE

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -3,16 +3,18 @@
 */
 
 public class Hourglass {
-	public static final int LINES = 4;
-	public static final int MAX = 12;
+	public static final int LINES /* of the glass */ = 4;
+	public static final int MAX = 2 * LINES + 2;
 	public static void main(String[] args) {
 		topLine();
 		topBody();
+		botBody();
+		topLine();
 	}
 	//TOP LINE CODE
 	public static void topLine() {
 		System.out.print("|");
-		for(int i = 1; i <= (MAX - 2); i++) {
+		for(int i = 1; i <= (MAX); i++) {
 			System.out.print("\"");
 		}
 		System.out.println("|");
@@ -20,14 +22,35 @@ public class Hourglass {
 	//TOP BODY CODE
 	public static void topBody() {
 		for(int i = 1; i <= LINES; i++) {
+				//PRINT SPACES
 				for(int j = 1; j <= i; j++) {
 					System.out.print(" ");
 				}
+				//PRINT GLASS
 				System.out.print("\\");
-				for(int k = 1; k <= -2 * MAX + 34; k++) {
+				for(int k = 1; k <= -2 * MAX + (-2 * i + 30); k++) {
 					System.out.print(":");
 				}
 				System.out.println("/");
+		}
+		//PRINT CENTER
+		for(int i = 1; i <= LINES + 1; i++) {
+			System.out.print(" ");
+		}
+		System.out.println("||");
+	}
+	//BOTTOM BODY CODE
+	public static void botBody() {
+		for(int i = 1; i <= LINES; i++) {
+			//PRINT SPACES
+			for(int j = 1; j <= -1 * i + 5; j++) {
+			System.out.print(" ");
+		}
+		System.out.print("/");
+		for(int k = 1; k <= 2 * MAX + (2 * i - 20); k++) {
+			System.out.print(":");
+		}
+		System.out.println("\\");
 		}
 	}
 }
